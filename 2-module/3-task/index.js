8d65418301c12ad088d4e0064c7d6425380e690d
Artem2088 <jablonsky.art@yndex.ru>
2023-01-26 12:55:17 +0300

    Модуль2, решение задач

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = +prompt("Введите первое число", 0);
+    this.b = +prompt("Введите второе число", 0);
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
