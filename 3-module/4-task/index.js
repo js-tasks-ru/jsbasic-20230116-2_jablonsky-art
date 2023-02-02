b4f9d2bf4c2c60ec791a7a289187712fb7bfbb14
Artem2088 <jablonsky.art@yndex.ru>
2023-02-02 20:12:14 +0300

    Исправление замечания

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,10 +1,14 @@
 function showSalary(users, age) {
-  let result = [];
-  users.forEach(function (item) {
-    if (item.age <= age) {
-      result.push(`${item.name}, ${item.balance}`);
-    }
-  });
+  // let result = [];
+  // users.forEach(function (item) {
+  //   if (item.age <= age) {
+  //     result.push(`${item.name}, ${item.balance}`);
+  //   }
+  // });
 
-  return result.join("\n");
+  // return result.join("\n");
+  return users
+    .filter((item) => item.age <= age)
+    .map((item) => `${item.name}, ${item.balance}`)
+    .join("\n");
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+  // for (let key of table.querySelectorAll("[data-available]")) {
+  //   let td = key.getAttribute("data-available");
+
+  //   let tr = key.parentElement;
+  //   let res = td
+  //     ? tr.classList.add("available")
+  //     : tr.classList.add("unavailable") || tr.classList.add("hidden");
+  // }
+  for (let tr of table.children[1].rows) {
+    // для каждой строки tr таблицы
+    let available = tr.cells[3];
+    if (available.hasAttribute("data-available"))
+      switch (available.dataset.available) {
+        case "true":
+          tr.classList.add("available");
+          break;
+        case "false":
+          tr.classList.add("unavailable");
+      }
+    else tr.hidden = true;
+
+    let gender = tr.cells[2].textContent;
+    switch (gender) {
+      case "m":
+        tr.classList.add("male");
+        break;
+      case "f":
+        tr.classList.add("female");
+    }
+    // Добавить inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18.
+    let age = +tr.cells[1].textContent;
+    if (age < 18) tr.style.textDecoration = "line-through";
+  }
 }
